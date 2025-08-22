85c3da7062c04a5d04ad0028e0f89d4ff99207ca
roshani2022 <rosh.gupta17@gmail.com>
2025-08-22 13:43:19 +0530

    Feat:Add html respone

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-   console.log(req);
-   process.exit();
-})
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //    process.exit();
+
+  res.setHeader("Content-Type", "text-html");
+  res.write("<html>");
+  res.write("<head> <title>This is my first page </title></head>");
+  res.write("<body>Hello from  Node.js server </body>");
+  res.write("</html>");
+  res.end();
+});
 
 server.listen(3000);
