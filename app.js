f9a5816eba1b14e89192e115924962d6bf8fdc8c
roshani2022 <rosh.gupta17@gmail.com>
2025-08-22 16:37:54 +0530

    Feat : Add Form page

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //    process.exit();
-
-  res.setHeader("Content-Type", "text-html");
+   const url = req.url
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head> <title>This is my first page </title></head>");
-  res.write("<body>Hello from  Node.js server </body>");
+  res.write("<body><h1>Hello from  Node.js server<h1> </body>");
   res.write("</html>");
   res.end();
 });
